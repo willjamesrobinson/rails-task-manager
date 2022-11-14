8899177720cce6a53fb2a2cc4922eb37d28eaa90
will robinson <willjamesr@gmail.com>
2022-11-14 15:37:13 +1100

    Task 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#index", as: :taskindex
+  get "tasks/new", to: "tasks#new", as: :tasknew
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :taskedit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
